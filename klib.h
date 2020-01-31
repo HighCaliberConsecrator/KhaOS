275c6b0e7038e3ff4e7b1109b2d6699d5a8a0f20
KevinR (HighCaliberConsecrator) <neometal@gmail.com>
2020-01-30 22:29:03 -0500

    Sort and swap lib functions

--- klib.h
+++ klib.h
@@ -13,7 +13,67 @@ namespace klib
         a = b;
         b = tmp;   
       }      
-    
+   
+
+
+   template <typename T>
+    void sort(T  &data,size_t start ,size_t end)
+      {
+        if(end - start < 2 )
+          {
+            //An array of 1 is already sorted, don't do anything  
+            return;
+          } 
+        
+        if( end-start < 8 )
+          {
+            //insertion sort when the array is small
+            auto max=data[start];
+            size_t maxkey=0;
+            for(size_t i=start;i < end; i++)
+              {
+                if(max < data[i] )
+                  {
+                    max = data[i];
+                    maxkey=i;
+                  }
+               }
+
+                klib::swap(data[maxkey],data[end-1]);
+               
+                klib::sort(data,start,--end);
+            return;
+          }
+       
+   
+         //otherwise this quicksort...
+         auto pivot = data[end-1];
+
+         size_t j = start;
+
+         for(size_t i=start; i < end; i++)
+           {
+             if(data[i] < pivot)
+               {
+                 klib::swap(data[i],data[j]);
+                 j++;                                    
+               }             
+
+           } 
+         
+             klib::swap(data[j],data[end-1]);
+
+         if( j > 1)
+           {  
+             klib::sort(data,start,--j);
+           }  
+         if(j<end)
+           {
+             klib::sort(data,++j,end);
+           }  
+         return; 
+      }        
+/*
     template <typename T>
     void sort(T  &data,size_t start ,size_t end)
       {
@@ -40,6 +100,7 @@ namespace klib
     
          return; 
       }        
+*/
   }          
 
 #endif
